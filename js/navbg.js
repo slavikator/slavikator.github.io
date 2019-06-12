c89c0dd5fc3f9ba399dfb4eb25bc39420d617286
slavikator <slavikator@gmail.com>
2019-06-12 13:32:22 +0300

    navbar transition

--- js/navbg.js
+++ js/navbg.js
@@ -1,6 +1,6 @@
 $(function() {
     $(window).on("scroll", function() {
-        if($(window).scrollTop() > 50) {
+        if($(window).scrollTop() > 32) {
             $("nav").addClass("navactive");
         } else {
             //remove the background property so it comes transparent again (defined in your css)
