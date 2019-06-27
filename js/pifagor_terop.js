978efe3405940ccae24cb543a8a8def41665c231
slavikator <slavikator@gmail.com>
2019-06-27 12:49:56 +0300

    pifagor ternary op

--- js/pifagor_terop.js
+++ js/pifagor_terop.js
@@ -3,9 +3,9 @@ for (var i = 1; i <= 9; i++) {
   row.setAttribute('class', 'row')
   for (var j = 1; j <=9; j++) {
     var cell = document.createElement('div');
-    @class = (i == j) ? 'cell, cell-main' : 'cell';
-    @class = (i > j) ? 'cell, cell-umain' : $class;
-    @class = (i < j) ? 'cell, cell-omain' : $class;
+    $class = (i == j) ? 'cell, cell-main' : 'cell';
+    $class = (i > j) ? 'cell, cell-umain' : $class;
+    $class = (i < j) ? 'cell, cell-omain' : $class;
     cell.setAttribute('class', $class);
     cell.innerText = i * j;
     row.appendChild(cell);
